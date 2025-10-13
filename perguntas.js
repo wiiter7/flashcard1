789c5e9ddc2f994f8b6dfc0c36936092f8df9d6f
wiiter7 <arthur.carneiro.santos@escola.pr.gov.br>
2025-10-13 08:53:09 -0300

    s

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,35 @@
+criaCartao(
+    'Futebol',
+    'Qual seleção tem mais copas do mundo?',
+    'Brasil'
+)
 
+criaCartao(
+    'Categoria',
+    'Pergunta',
+    'Resposta'
+)
+
+criaCartao(
+    'Categoria',
+    'Pergunta',
+    'Resposta'
+)
+
+criaCartao(
+    'Categoria',
+    'Pergunta',
+    'Resposta'
+)
+
+criaCartao(
+    'Categoria',
+    'Pergunta',
+    'Resposta'
+)
+
+criaCartao(
+    'Categoria',
+    'Pergunta',
+    'Resposta'
+)
