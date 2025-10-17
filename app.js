14108f7e3d9a9e0aa03eb4ec029c433a3461ef11
wiiter7 <arthur.carneiro.santos@escola.pr.gov.br>
2025-10-17 11:18:42 -0300

    s

--- app.js
+++ app.js
@@ -15,5 +15,13 @@ cartao.innerHTML = `
                     </div>
                 </div>
               `
+              let respostaEstaVisivel = false;
+
+              function viraCartao() {
+                respostaEstaVisivel = !respostaEstaVisivel
+                cartao.classList.toggle('active', respostaEstaVisivel)
+              }
+              cartao.addEventListener('click', viraCartao)
+              
     container.appendChild(cartao)          
 }
