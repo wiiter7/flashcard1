a5a4211c078aa32ddf8c47c334f96918ceb45986
wiiter7 <arthur.carneiro.santos@escola.pr.gov.br>
2025-10-17 07:59:16 -0300

    s

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 function criaCartao(categoria, pergunta, resposta) {
-    let container = document.getElementById('conteiner');
-    let cartao = document.createElement('article');
-    cartao.className = 'cartao'
+  let container = document.getElementById('container');
+  let cartao = document.createElement('article');
+  cartao.className = 'cartao';
 }
+
+cartao.innerHTML = 
+   <div class="cartao_conteudo">
+                    <h3>Programação</h3>
+                    <div class="cartao_conteudo_pergunta">
+                        <p>O que é javaScript?</p>
+                    </div>
+                    <div class="cartao_conteudo_resposta">
+                        <p>O javaScript é uma linguagem de Programação</p>
+                    </div>
+                </div>
+              
